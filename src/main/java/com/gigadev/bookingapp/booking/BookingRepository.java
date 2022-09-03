b96e991773089e0c345ce06774ba0c6e7c55a269
gi-ga-dev <cloud.gianluca@gmail.com>
2022-09-03 16:55:27 +0200

    abc

--- src/main/java/com/gigadev/bookingapp/booking/BookingRepository.java
+++ src/main/java/com/gigadev/bookingapp/booking/BookingRepository.java
@@ -1,9 +1,20 @@
 package com.gigadev.bookingapp.booking;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Long> {
+	
+	@Query("SELECT ele FROM Booking ele WHERE ele.user.id =:id AND ele.validity =:validity")
+	public List<Booking> findByUserAndValidity(Long id, LocalDate validity);
+		
+	@Query("SELECT ele FROM Booking ele WHERE ele.workstation.id =:id AND ele.validity =:validity")
+	public List<Booking> findByWorkstationAndValidity(Long id, LocalDate validity);
+		
 	
 }
