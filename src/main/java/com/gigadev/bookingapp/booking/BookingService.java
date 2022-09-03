0220985b2343867af76fe89ce4a9229ba588f1f7
gi-ga-dev <cloud.gianluca@gmail.com>
2022-09-03 16:56:53 +0200

    abc

--- src/main/java/com/gigadev/bookingapp/booking/BookingService.java
+++ src/main/java/com/gigadev/bookingapp/booking/BookingService.java
@@ -1,5 +1,6 @@
 package com.gigadev.bookingapp.booking;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,22 @@ public class BookingService {
 		return (List<Booking>) bookingRepo.findAll();
 	}
 	
-	public void create(Booking booking) {
-		bookingRepo.save(booking);
+	public List<Booking> searchByUserAndValidity(Long id, LocalDate validity) {
+		return bookingRepo.findByUserAndValidity(id, validity);
+	}
+	
+	public List<Booking> searchByWorkstationAndValidity(Long id, LocalDate validity) {
+		return bookingRepo.findByWorkstationAndValidity(id, validity);
+	}
+	
+	public void create(Booking booking) {	
+
+		List<Booking> bookings = searchByUserAndValidity(booking.getUser().getId(), booking.getValidity());		
+		List<Booking> bookingIfFree = searchByWorkstationAndValidity(booking.getWorkstation().getUniqueCode(), booking.getValidity());		
+	
+		if(bookings == null || bookings.size() == 0 && bookingIfFree == null || bookingIfFree.size() < booking.getWorkstation().getParticipants()) {
+			bookingRepo.save(booking);
+		}
 	}
 	
 	public Booking read(Long id) {
