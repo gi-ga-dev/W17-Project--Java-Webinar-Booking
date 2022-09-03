911ca2bf6ecafe7c3fe55e59c48a98e325ef8c36
gi-ga-dev <cloud.gianluca@gmail.com>
2022-09-03 17:15:33 +0200

    quasi final commit

---
+++ src/main/java/com/gigadev/bookingapp/BookingRunner.java
@@ -0,0 +1,15 @@
+package com.gigadev.bookingapp;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BookingRunner implements ApplicationRunner {@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		
+	}
+
+}
